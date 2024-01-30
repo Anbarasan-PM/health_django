61f6443f6a44fb2608f5f5a69666a5822b6b76b9
Anbarasan PM <81621442+Anbarasan-PM@users.noreply.github.com>
2024-01-30 09:21:29 +0530

    Delete auth directory

--- auth/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    aadhar_number = forms.CharField(max_length=12)
-    phone_number = forms.CharField(max_length=10)
--- auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import login, otp_verification
-
-app_name = 'auth'
-
-urlpatterns = [
-    path('login/', login, name='login'),
-    path('otp-verification/', otp_verification, name='otp_verification'),
-]
